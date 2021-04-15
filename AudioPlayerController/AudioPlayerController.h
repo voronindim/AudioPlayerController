aaed0d0a2f0097ea02b44ab7679b41031a774cf6
voronindim <51754697+voronindim@users.noreply.github.com>
2021-04-15 22:12:54 +0300

    Initial Commit

---
+++ AudioPlayerController/AudioPlayerController.h
@@ -0,0 +1,18 @@
+//
+//  AudioPlayerController.h
+//  AudioPlayerController
+//
+//  Created by Dmitrii Voronin on 15.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioPlayerController.
+FOUNDATION_EXPORT double AudioPlayerControllerVersionNumber;
+
+//! Project version string for AudioPlayerController.
+FOUNDATION_EXPORT const unsigned char AudioPlayerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioPlayerController/PublicHeader.h>
+
+
