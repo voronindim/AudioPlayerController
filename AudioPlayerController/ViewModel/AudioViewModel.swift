ab0d08ee466046933be213ea9deb7b01db5bb3c0
voronindim <51754697+voronindim@users.noreply.github.com>
2021-04-16 00:25:18 +0300

    Рефакторинг

--- AudioPlayerController/ViewModel/AudioViewModel.swift
+++ AudioPlayerController/ViewModel/AudioViewModel.swift
@@ -141,11 +141,9 @@ fileprivate extension CMTime {
         let hours:Int = Int(totalSeconds / 3600)
         let minutes:Int = Int(totalSeconds % 3600 / 60)
         let seconds:Int = Int((totalSeconds % 3600) % 60)
-
-        if hours > 0 {
-            return String(format: "%i:%02i:%02i", hours, minutes, seconds)
-        } else {
-            return String(format: "%02i:%02i", minutes, seconds)
-        }
+        
+        return hours > 0 ?
+            String(format: "%i:%02i:%02i", hours, minutes, seconds) :
+            String(format: "%02i:%02i", minutes, seconds)
     }
 }
